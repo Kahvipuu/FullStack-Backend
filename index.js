3f0319d1bf3fc10a2f8e4b663a63ce47e76c61f3
Kahvipuu <partaar@hotmail.com>
2021-02-19 15:00:18 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,100 +1,120 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const { request, response } = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-app.use(express.json())
+const Person = require('./models/person')
+
 app.use(express.static('build'))
+app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-    {
-        "name": "AP",
-        "number": "040",
-        "id": 5
-    },
-    {
-        "name": "AAP",
-        "number": "050",
-        "id": 6
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(p => {
+        response.json(p)
+    })
 })
 
+//hmm, mongoDBn koko jostain?
 app.get('/info', (request, response) => {
-    const info = '<p>Phonebook has info for ' + persons.length + ' people <br> ' + new Date() + '</p>'
+    const amount = Person.countDocuments(p => p == true)
+    const info = '<p>Phonebook has info for ' + amount + ' people <br> ' + new Date() + '</p>'
     response.send(info)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.send(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(p => {
+            if (p) {
+                response.send(p)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            next(error)
+        })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        persons = persons.filter(p => p.id !== id)
-        response.status(204).end()
-    } else {
-        response.status(404).end()
-    }
+    Person.findByIdAndDelete(request.params.id)
+        .then(r => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 
+    /*  vanha implementointi..
+        const id = Number(request.params.id)
+        const person = persons.find(p => p.id === id)
+        if (person) {
+            persons = persons.filter(p => p.id !== id)
+            response.status(204).end()
+        } else {
+            response.status(404).end()
+        }
+    */
 })
 
 app.post('/api/persons', (request, response) => {
     const personRequest = request.body
     if (personRequest.name && personRequest.number) {
-        if (persons.find(p => p.name === personRequest.name)) {
-            response.status(400).send({error: 'Name already in use'})
+        if (Person.find(p => p.name === personRequest.name)) {
+            response.status(400).send({ error: 'Name already in use' })
         } else {
+            const person = new Person({
+                name: personRequest.name,
+                number: personRequest.number,
+                //vanha, nyt mongo hoitaa => "id": Math.floor(Math.random() * 10000)
+            })
+            // ?hakeeko heti mongoDBstä uuden, vai miksi toimii heti?  persons = persons.concat(person)
+            person.save().then(savedPerson => {
+                response.json(savedPerson)
+            })
 
-
-            const person = {
-                "name": personRequest.name,
-                "number": personRequest.number,
-                "id": Math.floor(Math.random() * 10000)
-            }
-            persons = persons.concat(person)
-            response.json(person)
         }
     } else {
-        response.status(400).send({error: 'Name or number missing'})
+        response.status(400).send({ error: 'Name or number missing' })
     }
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(newP => {
+            console.log(newP)
+            response.json(newP)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    // siirto oletus virheenkäsittelijälle
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,46 @@
+// mongodb+srv://fullstack:<password>@hiekkaklusteri.33kk8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+    `mongodb+srv://fullstack:${password}@hiekkaklusteri.33kk8.mongodb.net/phonybook?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+    person.save().then(response => {
+        console.log('added person:', name, 'number:', number, 'to phonybook')
+        mongoose.connection.close()
+    })
+} else {
+    console.log('phooneboo:')
+    Person
+        .find({})
+        .then(ps => {
+            ps.forEach(p => {
+                console.log(p)
+            })
+            mongoose.connection.close()
+        })
+}
+
